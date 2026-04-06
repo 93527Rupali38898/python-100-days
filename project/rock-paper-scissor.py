d14cdad7015e1a1424f1bc27e18f4216f5b0f744
93527Rupali38898 <rupaligoyal382005@gmail.com>
2026-04-06 19:43:59 +0530

    updated the flow

--- project/rock-paper-scissor.py
+++ project/rock-paper-scissor.py
@@ -4,11 +4,13 @@ lst=[0, 1, 2] #rock, paper, scissor
 comp=random.choice(lst)
 print(comp)
 
+
 print("Welcome to Rock, Paper and Scissor Game!!")
 print("Enter number as your response: ")
 print("0 for Rock: ")
 print("1 for Paper")
 print("2 for Scissor")
+userName=input("Enter you name")
 user=int(input("Enter your Choice: "))
 print("Enter any other number to exit...")
 while(user==0 or user==1 or user==2):
@@ -20,3 +22,4 @@ while(user==0 or user==1 or user==2):
     else:
         print("Hurrah!! You Win")
     user=int(input("Enter your Choice: "))
+print(f"Have a nice day, dear {userName}")
